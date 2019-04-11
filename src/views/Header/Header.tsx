d220b28724c1f43f054e19179b0c521261bcf2c3
shawnkoon <shawn9405@gmail.com>
2019-04-10 19:45:36 -0700

    Finished implementing Header component will general landing message

--- src/views/Header/Header.tsx
+++ src/views/Header/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 }
 
 interface HeaderStates {
-  messageIndex?: number;
+  messageIndex: number;
   messages: string[];
 }
 
@@ -19,23 +19,39 @@ export class Header extends PureComponent<HeaderProps, HeaderStates> {
     this.state = {
       messageIndex: 0,
       messages: [
-        'Who am i',
-        'uhm... really?',
-        'Hello world',
-        'visit shawnkoon',
-        'I am blinking',
-        'something special',
-        'Please notice me',
+        'Welcome to CryptoKoon!',
+        'Try using Hash!',
+        'Or how about Encryption?',
+        'Check out my GitHub!',
+        '...',
+        'Have fun :)',
       ],
     };
   }
 
+  public handleMessageUpdate = () => {
+    this.setState(ps => ({
+      messageIndex: (ps.messageIndex + 1) % ps.messages.length,
+    }));
+  };
+
   render() {
+    const { messageIndex, messages } = this.state;
+    const currentMsg = messages[messageIndex];
+
     return (
-      <div className="d-flex flex-column">
-        <h1>CryptoKoon</h1>
-        <Typist avgTypingSpeed={200} startDelay={2000}>
-          <span>hi my name is shawn</span>
+      <div className="d-flex flex-column mt-5">
+        <h1 className="text-center mb-5 text-success">CryptoKoon</h1>
+        <Typist
+          key={messageIndex}
+          avgTypingDelay={this.props.textDelay}
+          className="text-center"
+          onTypingDone={this.handleMessageUpdate}
+        >
+          <span>{currentMsg}</span>
+          <Typist.Delay ms={1600} />
+          <Typist.Backspace count={currentMsg.length} />
+          <Typist.Delay ms={800} />
         </Typist>
       </div>
     );
