b06c4049f26d1c17831599c5abce93be02812049
shawnkoon <shawn9405@gmail.com>
2019-04-11 22:29:36 -0700

    Refactor contianer router logic for robust route

--- src/views/Container/Container.tsx
+++ src/views/Container/Container.tsx
@@ -1,6 +1,6 @@
 // Lib
 import * as React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 // App
 import 'styles/Container.scss';
@@ -9,10 +9,12 @@ export class Container extends React.PureComponent {
   render() {
     return (
       <div className="nav-container">
-        <Route path="/" exact component={() => <h1>I am root</h1>} />
-        <Route path="/hash" exact component={() => <h1>I am hash</h1>} />
-        <Route path="/encryption" exact component={() => <h1>I am encryption</h1>} />
-        <Route path="/decryption" exact component={() => <h1>I am decrpytion</h1>} />
+        <Switch>
+          <Route path="/hash" render={() => <h1>I am hash</h1>} />
+          <Route path="/encryption" render={() => <h1>I am encryption</h1>} />
+          <Route path="/decryption" render={() => <h1>I am decrpytion</h1>} />
+          <Route render={() => <Redirect to="/hash" />} />
+        </Switch>
       </div>
     );
   }
