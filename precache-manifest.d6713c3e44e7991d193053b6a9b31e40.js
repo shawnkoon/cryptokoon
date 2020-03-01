3b1dd5f919a3c802b8df1450870b8f25e471c6db
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-01 23:38:33 +0000

    Deploy shawnkoon/cryptokoon to github.com/shawnkoon/cryptokoon.git:gh-pages

---
+++ precache-manifest.d6713c3e44e7991d193053b6a9b31e40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5475ded01fd23a0ed2bd25935d12a131",
+    "url": "/cryptokoon/index.html"
+  },
+  {
+    "revision": "2c060a9c247cc46786ef",
+    "url": "/cryptokoon/static/css/main.5f8fff8e.chunk.css"
+  },
+  {
+    "revision": "ad2ea345e5fff21d8407",
+    "url": "/cryptokoon/static/js/2.1b0d1feb.chunk.js"
+  },
+  {
+    "revision": "9617c35fefb3909e96d4735aba67fb26",
+    "url": "/cryptokoon/static/js/2.1b0d1feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c060a9c247cc46786ef",
+    "url": "/cryptokoon/static/js/main.eec66818.chunk.js"
+  },
+  {
+    "revision": "8bfd5ead0b17acc7cfc6",
+    "url": "/cryptokoon/static/js/runtime-main.20d9aa36.js"
+  },
+  {
+    "revision": "78e7d370e0ff574fa0d4ca080e6d1b2d",
+    "url": "/cryptokoon/static/media/logo.78e7d370.png"
+  }
+]);
